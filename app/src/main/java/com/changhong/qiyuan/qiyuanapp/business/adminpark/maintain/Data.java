3da2faa777000589a1d16dec9bb75ded25f77d6e
mengyi <neutrino1997@foxmail.com>
2017-08-10 15:13:28 +0800

    整合了repair页面，将审核的不同整合到了一个页面，完成了recylerview的相关内容

---
+++ app/src/main/java/com/changhong/qiyuan/qiyuanapp/business/adminpark/maintain/Data.java
@@ -0,0 +1,7 @@
+package com.changhong.qiyuan.qiyuanapp.business.adminpark.maintain;
+
+
+public class Data {
+    static int repair_status=0;//0代表未审批，1代表已受理，2代表不受理
+    static int complaint_status=0;
+}
--- app/src/main/java/com/changhong/qiyuan/qiyuanapp/business/adminpark/maintain/RepairDetailActivity.java
+++ app/src/main/java/com/changhong/qiyuan/qiyuanapp/business/adminpark/maintain/RepairDetailActivity.java
@@ -1,29 +1,46 @@
 package com.changhong.qiyuan.qiyuanapp.business.adminpark.maintain;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.changhong.qiyuan.qiyuanapp.R;
 
 public class RepairDetailActivity extends AppCompatActivity {
-    private ImageButton bt_add;
-    private ImageButton bt_camera;
-    private ImageButton bt_photo;
+    private ImageButton ibn_add;
+    private ImageButton ibn_camera;
+    private ImageButton ibn_photo;
+    private Button bt_accpet;
+    private Button bt_refuse;
     private LinearLayout llayout_add;
+    private LinearLayout llayout_accpet;
+    private LinearLayout llayout_refuse;
+    private LinearLayout llayout_btn;
+    private RelativeLayout rlayout_edit;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busi_admp_repair_detail);
-        bt_add=(ImageButton) findViewById(R.id.ibn_busi_admp_repair_add);
+        rlayout_edit=(RelativeLayout)findViewById(R.id.rlayout_busi_admp_repair_edit);
+        llayout_accpet=(LinearLayout)findViewById(R.id.llayout_item_busi_admp_repir_accpet);
+        llayout_refuse=(LinearLayout)findViewById(R.id.llayout_item_busi_admp_repir_refuse);
+        llayout_btn=(LinearLayout)findViewById(R.id.llayout_item_busi_admp_button);
+
+        initlayout();
+        ibn_add=(ImageButton) findViewById(R.id.ibn_busi_admp_repair_add);
         llayout_add=(LinearLayout)findViewById(R.id.llayout_item_busi_admp_repir_add);
-        bt_add.setOnClickListener(new View.OnClickListener() {
+        ibn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(llayout_add.getVisibility()==View.GONE){
@@ -33,8 +50,8 @@ public class RepairDetailActivity extends AppCompatActivity {
             }
         });
 
-        bt_camera=(ImageButton)findViewById(R.id.ibn_busi_admp_repair_camera);
-        bt_camera.setOnClickListener(new View.OnClickListener() {
+        ibn_camera=(ImageButton)findViewById(R.id.ibn_busi_admp_repair_camera);
+        ibn_camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(); //调用照相机
@@ -43,13 +60,51 @@ public class RepairDetailActivity extends AppCompatActivity {
             }
         });
 
-        bt_photo=(ImageButton)findViewById(R.id.ibn_busi_admp_repair_photo);
-        bt_photo.setOnClickListener(new View.OnClickListener() {
+        ibn_photo=(ImageButton)findViewById(R.id.ibn_busi_admp_repair_photo);
+        ibn_photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);//调用图库
+                startActivity(intent);
 
             }
         });
 
+        bt_accpet=(Button)findViewById(R.id.btn_busi_admp_repair_accpet);
+        bt_accpet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RepairDetailActivity.this.finish();
+                Intent intent = new Intent(RepairDetailActivity.this,RepairInfoActivity.class);
+                startActivity(intent);
+                Data.repair_status=1;
+            }
+        });
+
+        bt_refuse=(Button)findViewById(R.id.btn_busi_admp_repair_refuse);
+        bt_refuse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RepairDetailActivity.this.finish();
+                Intent intent = new Intent(RepairDetailActivity.this,RepairInfoActivity.class);
+                startActivity(intent);
+                Data.repair_status=2;
+            }
+        });
+
+    }
+
+    private void initlayout() {
+        if(Data.repair_status==1) {
+            rlayout_edit.setVisibility(View.GONE);
+            llayout_btn.setVisibility(View.GONE);
+            llayout_accpet.setVisibility(View.VISIBLE);
+        }
+        if(Data.repair_status==2) {
+            rlayout_edit.setVisibility(View.GONE);
+            llayout_btn.setVisibility(View.GONE);
+            llayout_refuse.setVisibility(View.VISIBLE);
+        }
     }
 }
